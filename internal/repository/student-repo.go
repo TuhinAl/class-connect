10a4ca67cf40bdb106de039238b9077af88bb9c6
tuhinal <alauddintuhin1411@gmail.com>
2025-01-22 19:01:27 +0600

    persisting data  with SQL

--- internal/repository/student-repo.go
+++ internal/repository/student-repo.go
@@ -11,7 +11,7 @@ type StudentStore struct {
 }
 
 type StudentRepository interface {
-	Create(ctx context.Context, student *models.Student) error
+	Create(ctx context.Context, student *models.Student) (*models.Student, error)
 	// GetByID(ctx context.Context, id int64) (*models.Student, error)
 	// GetAll(ctx context.Context) ([]models.Student, error)
 	// Update(ctx context.Context, student *models.Student) error
@@ -20,6 +20,73 @@ type StudentRepository interface {
 	// GetByStudentID(ctx context.Context, studentID int64) (*models.Student, error)
 }
 
-func (studentStore *StudentStore) Create(ctx context.Context, student *models.Student) error {
-	return nil
+func (studentStore *StudentStore) Create(ctx context.Context, student *models.Student)  (*models.Student, error) {
+	
+	query := `INSERT INTO student_information (
+		student_id,
+		first_name,
+		last_name,
+		father_name,
+		phone,
+		gender,
+		course,
+		email,
+		is_active,
+		class_id,
+		password,
+		father_phone,
+		admission_fee,
+		total_fee,
+		remaining_fee,
+		monthly_fee
+	) VALUES (
+		$1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14, $15, $16
+	) RETURNING id, student_id, first_name, last_name, father_name, phone, gender,
+	  course, email, is_active, class_id, password, father_phone, admission_date, admission_fee, 
+	  total_fee, remaining_fee, monthly_fee
+	`
+err := studentStore.db.QueryRowContext(
+	ctx,
+	query,
+	student.StudentId,
+	student.FirstName,
+	student.LastName,
+	student.FatherName,
+	student.Phone,
+	student.Gender,
+	student.Course,
+	student.Email,
+	student.IsActive,
+	student.ClassID,
+	student.Password,
+	student.FatherPhone,
+	student.AdmissionFee,
+	student.TotalFee,
+	student.RemainingFee,
+	student.MonthlyFee,
+).Scan(
+	&student.ID,
+	&student.StudentId,
+	&student.FirstName,
+	&student.LastName,
+	&student.FatherName,
+	&student.Phone,
+	&student.Gender,
+	&student.Course,
+	&student.Email,
+	&student.IsActive,
+	&student.ClassID,
+	&student.Password,
+	&student.FatherPhone,
+	&student.AdmissionDate,
+	&student.AdmissionFee,
+	&student.TotalFee,
+	&student.RemainingFee,
+	&student.MonthlyFee,
+)
+
+if err != nil {
+	return nil, err
+}
+	return student, nil
 }
